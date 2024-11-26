babdb47cab495055100ae004ef0ac0ffcc180b57
Vasudev <79050603+Velstruck@users.noreply.github.com>
2024-11-26 19:14:31 +0530

    Added Redux and store

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
+import React, {useState,useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import {Header,Footer} from './components'
 import './App.css'
+import { Outlet } from 'react-router-dom'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .catch((error)=>{
+      console.log("Appwrite service :: getCurrentUser :: error",error);
+      dispatch(logout())
+    })
+    .finally(()=> setLoading(false))
+  },[])
   
 
-  return (
-    <>
-      <h1>Blog App with appwrite</h1>
-    </>
-  )
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+           {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
